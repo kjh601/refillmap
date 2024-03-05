47bf704eabb40f7f7bf0dedf99704fe7eefe3a5f
Algorithm <fighter0628@gmail.com>
2024-03-05 11:28:28 +0900

    refactor: 스프링 프로젝트 생성 후 정적 리소스 static 디렉터리로 이동

---
+++ src/main/java/toyproject/refillmap/RefillmapApplication.java
@@ -0,0 +1,13 @@
+package toyproject.refillmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefillmapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefillmapApplication.class, args);
+	}
+
+}
